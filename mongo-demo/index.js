87b37a1ea1232fc8b3044a37b24430f2db112e3b
sanketp25 <sanketpurohit25@gmail.com>
2024-05-03 17:19:04 -0500

    added update and delete

--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -39,11 +39,48 @@ async function getCourses(){
     // const courses = await Course.find();
     // console.log(courses);
     const coursesByFilter = await Course
+    //.find({price: { $gte: 10, $lte:20}})   // add >= and <= 
+    //.find({price: {$in: [10,15,20]}})
     .find({author:'Mosh',isPublished:true})
+    .or({author:'Mosh'}, {isPublished:true})
     .limit(10)
-    .sort({name : 1})
-    .select({name:1,tags:1});
+    .sort({name : 1}) //sort by column; 1 is asc, -1:desc
+    .select({name:1,tags:1}); //get the name and tag
+    //.count();
     console.log(coursesByFilter);
 }
 
+
 getCourses()
+//updating course using id by query first method
+async function updateCourse(id){
+    const course = await Course.findById(id);
+    if(!course){
+        return;
+    }
+    course.isPublished = false;
+    const result = await Course.save();
+    console.log(result);
+}
+
+//anothermedthod
+async function updateCourseByAnotherMethod(id){
+    const course = await Course.findByIdAndUpdate(id,{
+        $set:{
+            author:'Jack',
+            isPublished:false
+        }
+    },{new:true});
+    console.log(course);
+    // course.isPublished = false;
+
+}
+
+async function removeCourse(id){
+    //const result =await Course.deleteOne({_id: id}); //delete many  --> first way
+    const result = await Course.findByIdAndDelete(id) //returns null if not found
+}
+
+
+
+
