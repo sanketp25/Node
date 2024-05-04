50137abe3af80325001945329127459b54d00991
sanketp25 <sanketpurohit25@gmail.com>
2024-05-03 23:37:44 -0500

    added CUD for sub documents

--- mongo-demo/embedding.js
+++ mongo-demo/embedding.js
@@ -14,13 +14,13 @@ const Author = mongoose.model('Author', authorSchema);
 
 const Course = mongoose.model('Course', new mongoose.Schema({
   name: String,
-  author:authorSchema
+  authors: [authorSchema]
 }));
 
-async function createCourse(name, author) {
+async function createCourse(name, authors) {
   const course = new Course({
     name, 
-    author
+    authors
   }); 
   
   const result = await course.save();
@@ -32,20 +32,39 @@ async function listCourses() {
   console.log(courses);
 }
 
-//createCourse('Node Course', new Author({ name: 'Mosh' }));
+// createCourse('Node Course', [
+//   new Author({ name: 'Mosh' }),
+//   new Author({name: 'John'})
+// ]);
+
+async function addAuthor(courseId,author){
+  const course = await Course.findById(courseId);
+  course.authors.push(author);
+  course.save();
+}
 
 
+async function removeAuthor(courseId,authorId){
+  const course = await Course.findById(courseId);
+  const author = course.authors.id(authorId);
+  console.log(author)
+  author.deleteOne();
+  course.save();
+}
+
+//addAuthor('6635b6810a98b8f79e5e654d', new Author({name:'Amy'}))
+removeAuthor('6635b6810a98b8f79e5e654d','6635b6810a98b8f79e5e654b')
 // async function updateAuthor(id){
 //   const course = await Course.findById(id);
 //   course.author.name = 'Mosh Fraud';
 //   course.save();
 
 // }
-async function updateAuthor(courseId){
-  const course = await Course.updateOne({_id:courseId},{
-    $set:{
-      'author.name' : 'John Doe'
-    }
-  })
-}
-updateAuthor('6635ae0791da391934a222d5')
+// async function updateAuthor(courseId){
+//   const course = await Course.updateOne({_id:courseId},{
+//     $set:{ //use unset to unset the author or set  --> 'author': ''
+//       'author.name' : 'John Doe'
+//     }
+//   })
+// }
+// updateAuthor('6635ae0791da391934a222d5')
